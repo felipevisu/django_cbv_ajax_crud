64ee50811d833dd96d174086b8d21a748887ba56
Felipe Faria <felipevisu@gmail.com>
2020-01-14 00:01:47 -0300

    form style

--- apps/books/forms.py
+++ apps/books/forms.py
@@ -7,3 +7,6 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'pages', 'book_type']
+        widgets = {
+            'book_type': forms.RadioSelect(),
+        }
